64d26fea99d76d8fbb7583b0e5ba7767eb031b96
unknown <lgtablasf5@gmail.com>
2025-04-08 16:55:51 +0200

    EK-48 Login form created with styles but without functionality or error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { LoginForm } from './loginForm/loginForm.jsx';
 import './styles/styles.css';
+
 function App() {
   return (
     <>
-     <h1 className='text-3xl text-blue-500'>Hola React + Tailwind</h1>
+    <div>
+      {/* <h1 className='text-3xl text-blue-500'>Hola React + Tailwind</h1> */} 
+      <LoginForm /> 
+      </div>
     </>
   )
 }
