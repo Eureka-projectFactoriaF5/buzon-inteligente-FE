af67f76908c780dc13d75757430cddb02df74710
erikamc99 <erikamc99@gmail.com>
2025-04-08 13:16:43 +0200

    basic home structure without styles

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import { HomeButton } from "./HomeButton";
+
+export const Home = () => {
+  return (
+    <>
+        <HomeButton id="login-btn" text="Iniciar sesión" />
+        <HomeButton id="register-btn" text="Registrarse" />
+    </>
+  );
+}
---
+++ src/views/HomeView.jsx
@@ -0,0 +1,12 @@
+import { Home } from "../components/Home";
+
+export const HomeView = () => {
+  return (
+    <>
+        <div id="img-home">
+
+        </div>
+        <Home />
+    </>
+  );
+}
