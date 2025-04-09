3c6dbb26e381da7dfdee773cf2c4178fb9eb3d32
erikamc99 <erikamc99@gmail.com>
2025-04-09 09:44:05 +0200

    include header, img  and footer temporally

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -2,11 +2,15 @@ import { Home } from "../components/home/Home";
 
 export const HomeView = () => {
   return (
-    <div className="flex w-full h-full">
-        <div id="img-home" className="w-1/2">
-            {/* <img src="/img/Captura.PNG" alt="" className="bg-cover"/> */}
+    <>
+{/*         <Header /> */}
+        <div className="flex w-full h-full">
+            <div id="img-home" className="w-1/2">
+                {/* <img src="/img/Captura.PNG" alt="" className="bg-cover"/> */}
+            </div>
+            <Home />
         </div>
-        <Home />
-    </div>
+       {/*  <Footer /> */}
+    </>
   );
 }
