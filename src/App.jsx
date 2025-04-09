ba3634991695396a1c74c63aca43536924fcd34b
JesusEnjamio <jenjamiob@gmail.com>
2025-04-09 12:33:04 +0200

    hide navigation links on homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-
-
-import { FormularioRegistro } from './components/FormularioRegistro';
-import './styles/styles.css';
-import React from 'react';
+import { AppRouter } from "./routing/AppRouter";
+import "./styles/styles.css";
 
 function App() {
-
-
   
   return <AppRouter />;
-
-
 }
-
 
 export default App;
