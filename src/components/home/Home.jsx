b6515745e63b52fad7b1bc41ba0b942a34a1cb57
Erica <you@example.com>
2025-04-13 11:38:25 +0200

    Home routes added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,13 @@
 import { HomeButton } from "./HomeButton";
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
+  const navigate = useNavigate();
+  
   return (
     <div className="flex flex-col items-center gap-4  justify-center items-center w-full sm:w-1/2 my-7 sm:my-0">
-      <HomeButton id="login-btn" text="Iniciar sesión" />
-      <HomeButton id="register-btn" text="Registrarse" />
+      <HomeButton id="login-btn" text="Iniciar sesión" action={() => navigate("/login")} />
+      <HomeButton id="register-btn" text="Registrarse" action={() => navigate("/register")} />
     </div>
   );
 }
