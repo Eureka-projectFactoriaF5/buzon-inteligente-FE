f95ecad9c19dd106e4e92da957730e0eceddf6e4
Erica <you@example.com>
2025-04-13 13:21:38 +0200

    Home and HomeButton styles fixed

--- src/components/home/HomeButton.jsx
+++ src/components/home/HomeButton.jsx
@@ -3,7 +3,7 @@ export const HomeButton = ({action, text}) => {
 
   return (
     <button onClick={action} id="home-btn" 
-    className='w-1/4 lg:w-1/5 bg-[#4574FF] hover:bg-[#00328C] text-white font-bree text-xs py-1.5 sm:text-base sm:py-2 rounded-xl transition duration-200 cursor-pointer'>
+    className='w-1/4 lg:w-1/5 bg-[#4574FF] hover:bg-[#00328C] text-white font-bree text-xs py-1.5 sm:text-base sm:py-3 rounded-xl transition duration-200 cursor-pointer m-2'>
       {text}
     </button>
   );
