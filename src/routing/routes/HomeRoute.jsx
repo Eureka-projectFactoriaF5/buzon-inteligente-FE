42fed11c35131ef7d13067b35633a0abf9ef1bf8
Erica <you@example.com>
2025-04-13 13:56:47 +0200

    login route added to home, register route deleted because it's unnecessary, some styles changed to adapt to figma

--- src/routing/routes/HomeRoute.jsx
+++ src/routing/routes/HomeRoute.jsx
@@ -1,5 +1,6 @@
 import { HomeView } from "../../views/HomeView";
 import { RegisterForm } from "../../components/RegisterForm";
+{/* TODO: include login form import */}
 
 
 export const HomeRoute = {
@@ -14,5 +15,11 @@ export const HomeRoute = {
             path: "register",
             element: <RegisterForm />
         },
+/*      TODO: discomment this to include login form
+        {
+            path: "login",
+            element: <LoginForm />
+        }, 
+*/
     ]
 }  
