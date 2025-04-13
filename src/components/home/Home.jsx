b98a761dc01f724a661d091aa513fa654f3c0c99
Erica <you@example.com>
2025-04-13 13:21:38 +0200

    Home and HomeButton styles fixed

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,9 +3,9 @@ import { HomeButton } from "./HomeButton";
 export const Home = ({ onLoginClick, onRegisterClick }) => {
   
   return (
-    <div className="flex flex-col items-center gap-4  justify-center items-center w-full sm:w-1/2 my-7 sm:my-0">
+    <>
       <HomeButton id="login-btn" text="Iniciar sesión" action={onLoginClick} />
       <HomeButton id="register-btn" text="Registrarse" action={onRegisterClick} />
-    </div>
+    </>
   );
 }
